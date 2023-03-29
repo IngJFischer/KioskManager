911ffaf7ad109bfc127fed83f1632c09734b5a7b
Jeremy Fischer <ing_fischer@outlook.com>
2023-03-29 10:44:51 -0300

    Operation Controller

--- app/Http/Controllers/Api/OperationController.php
+++ app/Http/Controllers/Api/OperationController.php
@@ -11,10 +11,33 @@ use App\Http\Requests\Operation\StoreRequest;
 
 class OperationController extends Controller
 {
-    /** Devuelve un listado de las operaciones realizadas*/
+    /** Devuelve un listado de las operaciones realizadas
+     *  Si  Si la solucitud posee un encabezado indicando el parametro 'paginate'
+     *  devolvera los resultados paginados según las cantidades indicadas en dicho valor.
+     *  Si el parámetro 'paginate' no fue enviado, devolverá todas las operaciones.
+    */
     public function index(Request $request)
     {
-        //
+        //Revisar comentarios de funcionamiento en controlador del proveedor
+        if ($request->hasHeader('paginate'))
+        {
+            try {
+                $data = Operation::paginate($request->header('paginate'));
+            } catch (Throwable $error) {
+                return response()->json('Error durante la paginación', 400);
+            }
+        } 
+        else 
+        {
+            $data = Operation::get()->all();            
+        }
+        return response()->json($data, 200);
+    }
+
+    /** */
+    public function show(Operation $operation)
+    {
+        return response()->json($operation, 200);
     }
 
 
@@ -24,11 +47,6 @@ class OperationController extends Controller
         //
     }
 
-    /** */
-    public function show(Operation $operation)
-    {
-        //
-    }
 
     /** */
     public function update(Request $request, Operation $operation)
